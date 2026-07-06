3a59880d96854eff64a869e9caf636fd44606b44
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2026-07-06 16:52:17 +0900

    シーン切り替えを追加した。ゲームオーバー画面からタイトル、プレイに行くような切り替えを追加する

---
+++ Assets/Scripts/Scene/SceneSelector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // ✨シーン切り替えに必須のライブラリ
+
+public class SceneSelector : MonoBehaviour
+{
+    // 指定した名前のシーンに切り替える関数
+    public void ChangeScene(string sceneName)
+    {
+        // 💡 ゲームオーバー時などに時間が止まっている（Time.timeScale = 0）可能性があるので、1に戻しておく
+        Time.timeScale = 1f;
+
+        // シーンを読み込む
+        SceneManager.LoadScene(sceneName);
+    }
+}
